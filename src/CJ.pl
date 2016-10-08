cbd64b77a4d350f4e36cc0462aa17215f6677de8
Hatef Monajemi <monajemi@stanford.edu>
2016-10-08 16:33:39 -0700

    minor fix

--- src/CJ.pl
+++ src/CJ.pl
@@ -75,7 +75,7 @@ if( -d "$info_dir" ){
 
 
 # Dont sync if the command is one of these.
-my @nosync_cmds = qw ( init who help -help -h -Help -HELP prompt version -v );
+my @nosync_cmds = qw ( init who help -help -h -Help -HELP prompt version -v install-update);
 my %nosync = map { $_ => 1 } @nosync_cmds;
 
 
