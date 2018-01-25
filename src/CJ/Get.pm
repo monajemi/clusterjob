510191636f440e086c2f5210719df9ccad7c48d9
Hatef Monajemi <monajemi@stanford.edu>
2018-01-24 18:40:38 -0800

    Fixed Reduce problem as found by @xiaoyanh

--- src/CJ/Get.pm
+++ src/CJ/Get.pm
@@ -586,7 +586,8 @@ else
       firstline=\$(head -n 1 $completed_filename)
       COUNTER=`grep -o "[0-9]*" <<< \$firstline`
 
-      touch $res_filename;
+      mkdir -p "$(dirname "$res_filename")" && touch "$res_filename"
+    
       cat "\$COUNTER/$res_filename" > "$res_filename";
     
         # Pop the first line of remaining_list and add it to collect_list
