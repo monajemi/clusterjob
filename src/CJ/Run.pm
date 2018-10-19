c4dbcdbb2f97d9139e325723b55f346f2d013024
Hatef Monajemi <monajemi@stanford.edu>
2018-10-18 22:00:40 -0700

    removed print

--- src/CJ/Run.pm
+++ src/CJ/Run.pm
@@ -47,7 +47,7 @@ sub _update_qsub_extra {
     my $ssh = CJ::host($self->{machine});
     if( exists($ssh->{alloc}) and defined($ssh->{alloc}) ){
         if ( $ssh->{'alloc'} !~ /^[\"\'\t\s]*$/ ){
-        print "$ssh->{alloc} exists. I will supply these to qsub\n";
+            #print "$ssh->{alloc} exists. I will supply these to qsub\n";
         $self->{qsub_extra} = "$ssh->{alloc} $self->{qsub_extra}"; # append the user defined after default to take effect
         $self->{qSubmitDefault}=0;   # turn off CJ's default vals if users gives an alloc
         }
