47b4428b595d63bbf7f2b59b18f3d1e69f633461
bekkblando <bekkblando@gmail.com>
2019-05-16 13:43:44 -0700

    receive done, working on send

--- src/CJ/Hub.pm
+++ src/CJ/Hub.pm
@@ -6,6 +6,9 @@ use CJ;
 use CJ::CJVars;
 use CJ::Install;
 use Data::Dumper;
+use LWP::Simple;
+use JSON;
+
 
 
 sub new{
@@ -44,14 +47,53 @@ sub create_and_upload {
         PERL_MM_OPT="INSTALL_BASE=/home/ubuntu/perl5"; export PERL_MM_OPT;
     ';
     # TODO FIX THIS HARDCODING
-    my $run_upload = "cd $ssh->{remote_repo}/$program_name; perl $ssh->{remote_repo}/$program_name/upload_script.pm $CJID $pid $ssh->{remote_repo}/$program_name'";
+    my $run_upload = "cd $ssh->{remote_repo}/$program_name; perl $ssh->{remote_repo}/$program_name/upload_script.pm $CJID $pid $ssh->{remote_repo}/$program_name > ~/upload_log.txt'";
     &CJ::my_system($ssh_upload.' '.$env_var.' '.$run_upload, $verbose);
 }
 
 sub send {
     my $self = shift;
-    # Check that the PID hasn't been sent, should we override?
     $self->create_and_upload()
+}
+
+# sub share{
+#     my $self = shift;
+#     my ($shared_with) = @_;
+#     my $url = 'https://us-central1-united-pier-211422.cloudfunctions.net/share';
+    
+#     my %payload = (
+#         "cjid" => CJID,
+#         "cjpass" => CJKEY,
+#         "pid" => $self->{pid},
+#         "permission" => 1111,
+#         "shared_with" => $shared_with
+#     );
+    
+#     my $call = POST($url, Content => encode_json(\%payload), Content_Type => 'JSON(application/json)');
+# }
+
+sub receive{
+    my $self = shift;
+
+    system("mkdir $install_dir/receive");
+    my $url = "https://firebasestorage.googleapis.com/v0/b/united-pier-211422.appspot.com/o/$self->{pid}%2FEXPCJ.tar.gz?alt=media";
+    
+    my $efile = "$install_dir/receive/EXPCJ.tar.gz";
+
+    getstore($url, $efile);
+
+    $url = "https://firebasestorage.googleapis.com/v0/b/united-pier-211422.appspot.com/o/$self->{pid}%2FRESULTS.tar.gz?alt=media";
+
+    my $rfile = "$install_dir/receive/RESULTS.tar.gz";
+
+    getstore($url, $rfile);
+
+    system("mkdir $install_dir/receive/$self->{pid}");
+    system("tar -C $install_dir/receive -xvf $efile");
+    system("tar -C $install_dir/receive -xvf $rfile");
+    system("cd $install_dir/receive/; tar -zcvf $self->{pid}.tar.gz $self->{pid}");
+    system("rm $rfile $efile");
+
 }
 
 1;
