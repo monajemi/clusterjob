3ce6802c48c398a3ef6600940195796fb0c32226
Hatef Monajemi <monajemi@stanford.edu>
2019-09-23 17:00:17 -0700

    started anew to complete upload using curl. security of upload URL needs to take in CJKEY

--- src/server_scripts/upload_script_curl_template.sh
+++ src/server_scripts/upload_script_curl_template.sh
@@ -2,6 +2,11 @@
 # upload script for uploading CJ packages to
 # CJHub
 
+CJID="moosh"
+
+
+
+
 
 
 
@@ -18,7 +23,7 @@ local FILESIZE=$(wc -c $FILENAME | awk '{print $1}')
 # make an empty query
 local code=$(curl -v -w "%{http_code}" -X PUT -H 'Content-Length: 0' -H 'Content-Type: application/json' -H 'Content-Range: bytes */'$FILESIZE'' -d '' -o /dev/null $LOCATION_URL)
 
-# you can give an input for range as well
+# you can give an input for range of upload as well
 if [ $# -eq 3 ] ; then
   # calculate range
   myrange=0
@@ -35,43 +40,40 @@ echo $code
 upload_file(){
 #upload a given file and echo status
 
+
 local LOCATION_URL=$1
 local FILE=$2
 local __httpcode=$3 
+
 
 local http_code=$(get_status $LOCATION_URL $FILE)
 eval $__httpcode=$http_code
 
 
-
 if [[ $http_code -eq "308" ]] ; then 
 # resume upload
 
-  echo "     CJHub: $FILE : RESUMING!"
+  cjhub_message "$FILE : RESUMING!"
   
-  # FIXME: make name file dependent
-  local logname="upload_log.txt"
-  touch $logname
-  curl -sv -X PUT --upload-file $FILE $LOCATION_URL > $logname 2>&1
+ curl -sv -X PUT --upload-file $FILE $LOCATION_URL >$UPLOAD_LOG_FILE 2>&1
   
   if [[ $? -ne 0 ]] ; then
     # detected error
-    cat $logname 
+    cat $UPLOAD_LOG_FILE
     echo "     CJHub: $FILE : ERROR" 
     exit 1;
   fi
 
 elif [[ $http_code -eq 200 ]]; then
-
     echo "     CJHub: $FILE : COMPLETE!"
-
 else
     echo "     CJHub: HTTP code $http_code not recognized."
 fi
 }
 
 
-get_location_url() {
+
+create_location_url() {
 # variables that go into the cloud function
 # getSignedResUrl to get a location url
 # ex. 
@@ -79,46 +81,72 @@ get_location_url() {
 #   location_url=$(get_location_url moosh somedir test.tar)
 #echo $location_url
 
-CJID=$1
+
+local CJID=$1
 local PID=$2
 local FILENAME=$3
-
 # execute a function call for resumable url
-local LOCATION_URL=$(curl -X POST -H 'Content-Type: application/json' -d '{"pid":"'$PID'","cj_id":"'$CJID'","filename":"'"$FILENAME"'"}' 'https://us-central1-testcj-12345.cloudfunctions.net/getSignedResUrl' 2>/dev/null )
+# FIXME: This must require CJKey, otherwise, anyone can get an upload url
+local LOCATION_URL=$(curl -X POST -H 'Content-Type: application/json' -d '{"pid":"'$PID'","cj_id":"'$CJID'","filename":"'"$FILENAME"'"}' 'https://us-central1-testcj-12345.cloudfunctions.net/getSignedResUrl' 2>$UPLOAD_LOG_FILE) 
 
-# if error exit
-if [ $? -eq 0 ]; then
-    echo $LOCATION_URL > .cjhubloc
+if [[ ! -z $LOCATION_URL ]]; then
+        echo $LOCATION_URL > $(get_cjhubloc_name "$CJID" "$PID" "$FILENAME")
 else
-    echo $LOCATION_URL
-    exit 1;
+      cat $UPLOAD_LOG_FILE
+      cjhub_message "Failed to create upload location url for $PID/$FILENAME" 
+      exit 1;
 fi
+
+}
+
+rm_extension(){
+local fullfile=$1
+local filename=$(basename -- "$fullfile")
+#local extension="${filename##*.}"
+filename="${filename%.*}"
+echo $filename
+}
+
+get_cjhubloc_name(){
+
+local CJID=$1
+local pid=$2
+local filename=$3
+echo ".cjhubloc_"$CJID"_"$pid"_"$(rm_extension "$filename")
+}
+
+get_upload_log_filename() {
+ echo ".upload_log_"$CJID"_"$pid"_"$(rm_extension "$filename")".txt"
 }
 
 
 
 
-
-
-
-
+cjhub_message(){
+echo "          CJhub: $@"
+}
 
 #filename="test.tar"
+
+pid="somedir"
 filename="0854a767f859fda5b879edc8f49634f1cf58bfba.tar"
 # exec upload
-[[ ! -f '.cjhubloc' ]] && location_url=$(get_location_url moosh somedir $filename)
+CJHUBLOC_FILE=$(get_cjhubloc_name "$CJID" "$pid" "$filename")
+UPLOAD_LOG_FILE=$(get_upload_log_filename "$CJID" "$pid" "$filename")
 
-location_url="`cat .cjhubloc`"
-upload_file $location_url "somedir/$filename" code
+if [[ ! -f $CJHUBLOC_FILE ]] ; then 
+  cjhub_message "Creating loc url for $pid/$filename"
+  create_location_url "$CJID" "$pid" "$filename"
+else
+  cjhub_message "Retreiving loc url to resume $pid/$filename"
+fi
+
+location_url="`cat $CJHUBLOC_FILE`"
+upload_file "$location_url" "$pid/"$filename "code"
 
 #code=308
 #while [[ $code -eq 308 ]];do
 #  upload_file $location_url "somedir/"$filename code
 #  echo $code
 #done
-
-
-
-
-
 
